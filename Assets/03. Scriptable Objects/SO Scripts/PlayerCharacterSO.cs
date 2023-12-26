dceb9f8ad048943b6d337762f553dcda62dd0765
VictorJo323 <59429114+VictorJo323@users.noreply.github.com>
2023-12-26 20:20:40 +0900

    [ADD] Assets & Scene Management Script Added

---
+++ Assets/03. Scriptable Objects/SO Scripts/PlayerCharacterSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum StanceType 
+{
+    attack,
+    counter,
+    throwing,
+    asisst, 
+}
+
+[CreateAssetMenu(fileName = "Character", menuName = "Player/Character", order =0)]
+public class PlayerCharacterSO : ScriptableObject
+
+{
+    
+}
